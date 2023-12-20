bb7830848092f3eb782be674410dec445da4fd41
LeonardoScatolin <leonardo.scatolin20@gmail.com>
2023-12-20 00:31:19 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,5 +35,11 @@ public class Program {
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
 
+        System.out.println("\n=== TEST 5: seller update ===");
+        seller = sellerDao.findById(9);
+        seller.setName("Leonardo");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
+
     }
 }
